ce3fd300df6fb0f76d49d5e2671fea92509c1a21
Idris <dri.taha24@gmail.com>
2024-02-28 21:19:04 +0300

    Updating node packages and installed dependencies, adding getBalanceOf() func

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,49 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const ethers_1 = require("ethers");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const apiKey = process.env.etherscanApiToken;
-console.log(apiKey);
+const provider = new ethers_1.ethers.EtherscanProvider("homestead", apiKey);
+const usdtContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+const usdtAbi = [
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "who",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+const usdtContract = new ethers_1.ethers.Contract(usdtContractAddress, usdtAbi, provider);
+function getBalanceOf(address) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const balance = yield usdtContract.balanceOf(address);
+        // Convert from wei to usdt
+        const balanceInUsdt = ethers_1.ethers.formatUnits(balance, 6);
+        console.log(`Balance: ${balanceInUsdt} USDT`);
+    });
+}
+getBalanceOf("0xdac17f958d2ee523a2206206994597c13d831ec7");
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,39 @@ import { config } from 'dotenv';
 config()
 
 const apiKey = process.env.etherscanApiToken
-console.log(apiKey)
+
+const provider = new ethers.EtherscanProvider("homestead", apiKey);
+const usdtContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+const usdtAbi = [
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "who",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+const usdtContract = new ethers.Contract(usdtContractAddress, usdtAbi, provider);
+
+async function getBalanceOf(address:string) {
+    const balance = await usdtContract.balanceOf(address)
+    // Convert from wei to usdt
+    const balanceInUsdt = ethers.formatUnits(balance, 6);
+    
+    console.log(`Balance: ${balanceInUsdt} USDT`)
+}
+
+getBalanceOf("0xdac17f958d2ee523a2206206994597c13d831ec7")
